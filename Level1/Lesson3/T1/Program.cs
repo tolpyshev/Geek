49ebc1dac6febddfec233642ce751e546cdd2390
Svyatoslav <tolpyshev.si@yandex.ru>
2021-02-28 01:20:01 +0300

    67

---
+++ Level1/Lesson3/T1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string greeting = "Hello, world!";
+            for (int i = 0; i < greeting.Length; i++)
+            {
+                Console.WriteLine(greeting[i]); // построчный вывод каждого символа строки
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            int[,] dmass = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            for (int i = 0; i < dmass.GetLength(0); i++)
+            {
+                for (int j = 0; j < dmass.GetLength(1); j++)
+                {
+                    Console.WriteLine($"{dmass[i, j]}");
+                }
+            }
+        }
+    }
+}
