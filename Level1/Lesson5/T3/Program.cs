4d518f84da19c914c132df4a374321215527a217
Svyatoslav <tolpyshev.si@yandex.ru>
2021-03-03 21:05:14 +0300

    L5T1-3commit

---
+++ Level1/Lesson5/T3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+namespace T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(new FileStream("numbers.bin", FileMode.OpenOrCreate), s);
+
+        }
+    }
+}
