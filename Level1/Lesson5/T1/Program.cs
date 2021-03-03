d30ddeb76f8d937bb7145baaf68718cc11cccff3
Svyatoslav <tolpyshev.si@yandex.ru>
2021-03-03 18:47:01 +0300

    L5T1commit

---
+++ Level1/Lesson5/T1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string OutFile = "Челобитная.txt";
+            File.WriteAllText(OutFile, "Очень добрый день Артём, разрешите сдать третий уроки, по семейным обстоятельствам, независящим от меня, не успевал его сдать. Ну пожааалуйста. ");
+        }
+    }
+}
