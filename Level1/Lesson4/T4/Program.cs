c68d163bf36c9bf094b36df2f9e61833cc732dca
Svyatoslav <tolpyshev.si@yandex.ru>
2021-03-03 11:00:03 +0300

    k4t4commit

---
+++ Level1/Lesson4/T4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace T4
+{
+    class Program
+    {
+        static int Fibo(int num)
+        {
+
+            if (num < 2)
+            {
+                return num;
+            }
+            return Fibo(num - 1) + Fibo(num - 2);
+
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            int FiboNum = Fibo(Num);
+           
+            Console.WriteLine(FiboNum);
+
+
+        }
+    }
+}
+
